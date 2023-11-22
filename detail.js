d0f0d73f692fca0836748f4eb57902fb56eeb87a
minseoi <minseoi.cc@gmail.com>
2023-11-22 11:20:26 +0900

    Init

---
+++ detail.js
@@ -0,0 +1,33 @@
+window.addEventListener('DOMContentLoaded', function() {
+  urlSearch = new URLSearchParams(location.search);
+  id = urlSearch.get('id')
+  RequestDrawing(id);
+});
+
+function RequestDrawing(id) {
+  fetch("http://ise.kro.kr:8123/api/drawing?id="+id, {
+    method: "GET"
+  }) 
+  .then(res => res.json())
+  .then(json => DrawingCallback(json))
+  .catch(error => console.error("Error:", error));
+}
+
+function DrawingCallback(json) {
+  const author = json.author;
+  const title = json.title;
+  const desc = json.desc;
+  const imageUrl = json.imageUrl;
+
+  const galleryImage = document.querySelector('.gallery_image');
+  galleryImage.src = imageUrl;
+
+  const galleryTitle = document.querySelector('.gallery_title');
+  galleryTitle.innerText = title;
+
+  const galleryDesc = document.querySelector('.gallery_desc');
+  galleryDesc.innerText = desc;
+
+  const galleryAuthor = document.querySelector('.gallery_author');
+  galleryAuthor.innerText = author;
+}
